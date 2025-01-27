5f42decca52ff954a4c57cd07d4f1f0079d4deae
Marwa Alazab <104984704+Merve-1@users.noreply.github.com>
2025-01-27 22:52:18 +0200

    update

--- backend/controllers/providers.js
+++ backend/controllers/providers.js
@@ -22,5 +22,19 @@ module.exports.edit = function(req,res){
 module.exports.update = function(req,res){
     let id= req.params.id;
     let provider = providers.find(provider => provider.id == id);
+    provider.firstname = req.body.firstname;
+    provider.lastname = req.body.lastname;
+    provider.position = req.body.position;
+    provider.company.company_name = req.body.company_name;
+    provider.company.address = req.body.address;
+    provider.company.address2 = req.body.address2;
+    provider.company.city = req.body.city;
+    provider.company.state = req.body.state;
+    provider.company.postal_code = req.body.postal_code;
+    provider.company.phone = req.body.phone;
+    provider.company.email = req.body.email;
+    provider.company.description = req.body.description;
+    provider.company.tagline = req.body.tagline;
+
     res.render('providers/providers-update',{title: 'Service Providers Update'})
 }
